90f75acc1776f15cc76440e658058fe941ffae92
Saman Miran <c@samanmiran.com>
2023-10-05 01:33:18 +0200

    Restore main

--- source/main.cpp
+++ source/main.cpp
@@ -1,22 +1,18 @@
 
-//#include "application.h"
-#include "ecs/entity_manager.h"
+#include "application.h"
 #include <iostream>
 
 int main() {
 
-    ECS::testEntityManager();
-    ECS::benchmark();
+    Doughnut::Application app{};
+    app.title = "Hello World!";
 
-//    Doughnut::Application app{};
-//    app.title = "Hello World!";
-//
-//    try {
-//        app.run();
-//    } catch (const std::exception &e) {
-//        std::cerr << e.what() << std::endl;
-//        return EXIT_FAILURE;
-//    }
+    try {
+        app.run();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
