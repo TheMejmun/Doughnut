3732a2ee3c0357c778d18967fabe55cba8ff8ea6
Saman Miran <c@samanmiran.com>
2023-10-07 12:03:34 +0200

    value -> dereference

--- include/ecs/entity_manager.h
+++ include/ecs/entity_manager.h
@@ -99,7 +99,7 @@ namespace ECS2 {
                 componentVector<COMPONENT>().emplace_back(component);
             } else {
                 verbose("Overriding component " << typeid(COMPONENT).name());
-                uint32_t componentId = mIndexVectors[componentVectorId][denseId].value();
+                uint32_t componentId = *mIndexVectors[componentVectorId][denseId];
                 componentVector<COMPONENT>()[componentId] = std::move(component);
             }
         }
@@ -123,10 +123,10 @@ namespace ECS2 {
                 auto currentLastIndex = componentVector<COMPONENT>().size() - 1;
 
                 // Move current rear to deleted position and updated other entity with new component position
-                componentVector<COMPONENT>()[componentIndex.value()] = componentVector<COMPONENT>().back();
+                componentVector<COMPONENT>()[*componentIndex] = componentVector<COMPONENT>().back();
                 for (std::optional<uint32_t> &otherIndex: mIndexVectors[typeIndex]) {
-                    if (otherIndex.has_value() && otherIndex.value() == currentLastIndex) {
-                        otherIndex = componentIndex.value();
+                    if (otherIndex.has_value() && *otherIndex == currentLastIndex) {
+                        otherIndex = *componentIndex;
                         break;
                     }
                 }
@@ -148,7 +148,7 @@ namespace ECS2 {
 
             assert(componentIndex.has_value());
 
-            return componentVector<COMPONENT>()[componentIndex.value()];
+            return componentVector<COMPONENT>()[*componentIndex];
         }
 
 
@@ -266,7 +266,7 @@ namespace ECS2 {
         template<class T>
         inline void insertArchetypeComponents(std::vector<T *> &cRefs, uint32_t denseIndex, uint32_t archetypeIndex) {
             const auto typeIndex = mTypeIndexMap[std::type_index(typeid(T))];
-            const uint32_t componentIndex = mIndexVectors[typeIndex][denseIndex].value();
+            const uint32_t componentIndex = *mIndexVectors[typeIndex][denseIndex];
 
             cRefs[archetypeIndex] = &componentVector<T>()[componentIndex];
         }
