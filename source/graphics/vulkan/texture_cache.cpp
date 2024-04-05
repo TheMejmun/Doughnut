7fc0f130f61afdfee2c35a708956c3026bbd3d0a
Saman Miran <c@samanmiran.com>
2024-04-05 22:04:56 +0200

    cleanup

--- source/graphics/vulkan/texture_cache.cpp
+++ source/graphics/vulkan/texture_cache.cpp
@@ -26,7 +26,6 @@ void TextureCache::preload(const std::string &texture) {
 void TextureCache::preload(const Texture &texture) {
     std::lock_guard<std::mutex> guard{mInsertTextureMutex};
     if (!mRenderTextures.contains(texture.mFilename)) {
-        vk::Extent2D extent{static_cast<uint32_t>(texture.mWidth), static_cast<uint32_t>(texture.mHeight)};
 
         // https://stackoverflow.com/questions/68828864/how-can-you-emplace-directly-a-mapped-value-into-an-unordered-map
         mRenderTextures.try_emplace(
